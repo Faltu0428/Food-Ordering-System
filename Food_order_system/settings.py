525e8085e743171270d5093c9af59aea0325ad58
Faltu0428 <faltu0428@gmail.com>
2024-08-03 10:47:27 +0530

    Update settings.py

--- Food_order_system/settings.py
+++ Food_order_system/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'Food_order_system.urls'
